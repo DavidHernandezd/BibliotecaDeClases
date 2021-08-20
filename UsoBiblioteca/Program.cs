855ba731dad5236c307b4281424b43f4c15f7072
DavidHernandezd <dh785246@gmail.com>
2021-08-19 19:45:28 -0600

    Proyecto ejemplo biblioteca de clases

---
+++ UsoBiblioteca/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BiblioteclaClases;
+
+namespace UsoBiblioteca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //llamado al metodo SortedListCollection de la clase CollectionExample
+            //De nuestro ensamblado 
+            CollectionExample.SortedListCollection();
+            Console.ReadKey();
+
+        }
+    }
+}
